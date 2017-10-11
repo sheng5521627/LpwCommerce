f3b294332ae3e3e453ba44d752a5e9bb6b84402c
lpw <lpw-157@163.com>
2017-10-11 18:07:40 +0800

    第一次提交

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nopConfi = ConfigurationManager.GetSection("NopConfig") as NopConfig; 
+
+            Console.WriteLine(nopConfi.IgnoreStartupTasks);
+            Console.ReadLine();
+        }
+    }
+}
