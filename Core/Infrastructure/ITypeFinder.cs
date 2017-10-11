beb66f0b3691df0973a6ff658d2652cf3b0a4108
lpw <lpw-157@163.com>
2017-10-11 18:11:55 +0800

    下班回家

---
+++ Core/Infrastructure/ITypeFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure
+{
+    /// <summary>
+    /// 查找对饮的类型
+    /// </summary>
+    public interface ITypeFinder
+    {
+        IList<Assembly> GetAssemblies();
+    }
+}
