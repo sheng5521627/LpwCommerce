0537a8d3976071a7b4a7b4733260af4106e2a14b
sheng5521627 <lpw-157@163.com>
2017-10-11 22:25:27 +0800

    休息

--- Core/Infrastructure/ITypeFinder.cs
+++ Core/Infrastructure/ITypeFinder.cs
@@ -13,5 +13,13 @@ namespace Core.Infrastructure
     public interface ITypeFinder
     {
         IList<Assembly> GetAssemblies();
+
+        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
     }
 }
