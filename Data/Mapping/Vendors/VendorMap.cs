4f9a1c8f651e845aa34aeb90a85cd8874523e1c7
sheng5521627 <lpw-157@163.com>
2017-10-22 16:24:39 +0800

    周末在家

---
+++ Data/Mapping/Vendors/VendorMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Vendors;
+
+namespace Data.Mapping.Vendors
+{
+    public partial class VendorMap:NopEntityTypeConfiguration<Vendor>
+    {
+        public VendorMap()
+        {
+            this.ToTable("Vendor");
+            this.HasKey(m => m.Id);
+            this.Property(m => m.Name).IsRequired().HasMaxLength(400);
+            this.Property(m => m.Email).HasMaxLength(400);
+            this.Property(m => m.MetaKeywords).HasMaxLength(400);
+            this.Property(m => m.MetaTitle).HasMaxLength(400);
+            this.Property(m => m.PageSizeOptions).HasMaxLength(200);
+        }
+    }
+}
